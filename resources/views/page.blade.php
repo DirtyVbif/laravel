40f6a56d791e62e7a18b4e179831c99b0bd78ca1
DirtyMike <uspmik@gmail.com>
2020-06-28 20:16:04 +0300

    update footer content and styles

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -56,7 +56,10 @@
 
   <footer class="page-footer">
     @section('footer')
-      <div class="container container_footer">All rights reserved &copy; 2020 by Michael Uspensky</div>
+      <div class="container container_footer">
+        <div class="container_footer__line">All rights reserved &copy; 2020 by <a href="https://vk.com/dirty.mike" target="_blank" class="footer-link">Michael Uspensky</a></div>
+        <div class="container_footer__line"><a href="https://github.com/DirtyVbif/laravel" target="_blank" class="footer-link">This project on git</a></div>
+      </div>
     @show
   </footer>
 

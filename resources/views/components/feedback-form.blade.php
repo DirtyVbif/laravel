c9bac6d2c195f68704f4daa5afad0e110aef5407
DirtyMike <uspmik@gmail.com>
2020-07-05 20:55:21 +0300

    add temporary button into feedback from to see current feedbacks

--- resources/views/components/feedback-form.blade.php
+++ resources/views/components/feedback-form.blade.php
@@ -14,7 +14,8 @@
     <textarea name="body" class="form__input form__input_area" placeholder="Type your comment" required>{{ old('body') }}</textarea>
   </div>
 
-  <div class="form__row form__row_footer">
+  <div class="form__row form__row_footer flex flex_row flex_space-around">
     <input type="submit" class="form__submit button button_solid" value="Send feedback">
+    <a href="/api/feedbacks" class="button button_ghost">Current feedbacks</a>
   </div>
 </form>
